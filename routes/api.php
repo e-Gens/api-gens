b0550cafbf8522c0895583a248f561cdbfb101ba
Elias Alves <unixelias@gmail.com>
2018-10-17 00:27:54 -0300

    Login iniciado
    Adiciona tradução do Laravel 5.6

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,53 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/criar-usuario', function (Request $request) {
+    $data = $request->all();
+
+    $validacao = Validator::make($data, [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+    ]);
+
+    if ($validacao->fails()) {
+        return $validacao->errors();
+    };
+
+    $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    $user->token = $user->createToken($user->email)->accessToken;
+
+    return $user;
+});
+
+Route::post('/login', function (Request $request) {
+    $data = $request->all();
+    $validacao = Validator::make($data, [
+        'email' => 'required|string|email|max:255',
+        'password' => 'required|string|min:3',
+    ]);
+
+    if ($validacao->fails()) {
+        return $validacao->errors();
+    };
+
+    if (Auth::attempt(['email' => $data['email'], 'password' => $data['password']])) {
+        $user = auth()->user();
+        $user->token = $user->createToken($user->email)->accessToken;
+        return $user;
+    } else {
+        return ['status'=>false];
+    }
+
+});
+
+
+
+/* Faz a verificação de login com o token do usuário, retornando o usuário*/
+Route::middleware('auth:api')->get('/usuario', function (Request $request) {
     return $request->user();
 });
