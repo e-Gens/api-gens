b728226884d8b342c670ee22916044f1be16904f
Elias Alves <unixelias@gmail.com>
2018-10-26 10:31:14 -0300

    Novo deploy no servidor

--- deploy.sh
+++ deploy.sh
@@ -5,35 +5,24 @@
 # sshpass => programa para entrada de senha no sftp
 # -p [] => senha
 # --------------------------------------------------
-# sftp => conexão via ssh para upload de arquivos
-# -P [] => porta da conexão
+# ssh => conexão via ssh com o servidor
+# -p [] => porta da conexão
 # -v => (verbose) com log de execução
 # -o => opção ssh - sem validação restrita por chaves
 # --------------------------------------------------
 
-# remove node_modules
-rm -rf node_modules;
-# remove tests
-rm -rf tests;
-
-# acessa o diretório correto
-cd $TRAVIS_BUILD_DIR ;
-cd .. ;
 
 # loga no ssh
-sshpass -p $FTP_PASSWD sftp -P $FTP_PORT -v -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST <<END_SCRIPT
-progress
-rm $REMOTE_ROOT/$LOCAL_ROOT/$FILE
-
-progress
-put -r $LOCAL_ROOT $REMOTE_ROOT
-bye
+sshpass -p $FTP_PASSWD ssh -v -p $FTP_PORT -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST <<END_SCRIPT
+cd api-gens;
+git pull;
+composer install --no-interaction --no-dev --optimize-autoloader
 END_SCRIPT
 exit 0
 # ==================================================
-# rm => remove arquivos no servidor
-# put => envia arquivos ao servidor
+# cd api-gens; => entra no diretório base da api
+# git pull; => baixa as atualizações pelo git
 # progress => exibe o progresso
-# bye => encerra a conexão
+# composer install ... => verifica deps e instala
 # --------------------------------------------------
 
