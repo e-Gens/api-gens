95deecd4f930281d9a2d85533c74cdf9fbaaaf43
Elias Alves <unixelias@gmail.com>
2018-10-26 12:21:22 -0300

    Separa script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,14 @@
 # Variáveis via Travis Secrets
 
 # ==================================================
+# cd api-gens; => entra no diretório base da api
+# git pull; => baixa as atualizações pelo git
+# progress => exibe o progresso
+# composer install ... => verifica deps e instala
+# --------------------------------------------------
+SCRIPT="cd api-gens; git pull; composer install --no-interaction --no-dev --optimize-autoloader"
+
+# ==================================================
 # sshpass => programa para entrada de senha no sftp
 # -p [] => senha
 # --------------------------------------------------
@@ -10,19 +18,11 @@
 # -v => (verbose) com log de execução
 # -o => opção ssh - sem validação restrita por chaves
 # --------------------------------------------------
-
-
-# loga no ssh
-sshpass -p $FTP_PASSWD ssh -v -p $FTP_PORT -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST <<END_SCRIPT
-cd api-gens;
-git pull;
-composer install --no-interaction --no-dev --optimize-autoloader
-END_SCRIPT
+if [ "${TRAVIS_BRANCH}" = "develop" ] # Apenas se [branch]
+then
+    # loga no ssh e executa o script
+    sshpass -p $FTP_PASSWD ssh -v -p $FTP_PORT -o StrictHostKeyChecking=no $FTP_USER@$FTP_HOST $SCRIPT
+fi
 exit 0
-# ==================================================
-# cd api-gens; => entra no diretório base da api
-# git pull; => baixa as atualizações pelo git
-# progress => exibe o progresso
-# composer install ... => verifica deps e instala
-# --------------------------------------------------
+
 
